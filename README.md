2145881072a7348ed6b57a5a86eefcda1cd8dbca
Lucas de Linhares <lucasdelinhares@gmail.com>
2023-05-12 02:45:55 +0100

    Update ao README e dar setup aos pins

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # PAP-Cocktail-Automtico
-Código, Fotos, Modelos 3D, Relatório e Apresentação
+<div align="center">
+    <img src="./Fotos/imagem_modelo3d.png" />
+</div>
+
+
+## Descrição
+
+Este repositório contém todos os recursos relacionados ao projeto de PAP (Projeto de Aptidão Profissional) de um Cocktail Automático, desenvolvido por, Lucas de Linhares e Roberto Fernandes.  
+A PAP é um projeto que desenvolvemos como parte do processo de formação profissional.    Esta inclui vários elementos, como um sistema de mistura de liquidos, um WebSite, pombas peristálticas e mais!
+
+
+
+## Instalação do WebSite
+
+```bash
+# Clone o repositório
+git clone https://github.com/lucascompython/PAP-Cocktail-Automatico
+
+# Entre no diretório do repositório
+cd PAP-Cocktail-Automatico/WebSite
+
+# Instale as dependências
+pnpm install
+
+# Inicie o servidor de desenvolvimento
+pnpm run dev
+
+# Para gerar uma build de produção
+pnpm run build
+
+##############
+
+# Instalar as dependências do backend
+
+# Entre no diretório do backend
+cd ../Raspberry
+
+# Instale as dependências
+pip3 install fastapi uvicorn[standard]
+
+# Inicie APENAS o servidor de backend
+uvicorn api:app --reload
+
+# Inicie o programa inteiro (incluindo o controle dos motores)
+python3 main.py
+
+
+```
+
+
+## Licenciamento
+
+Acreditamos no poder do software de código aberto para impulsionar a inovação e tornar o mundo um lugar melhor. Como tal, estamos empenhados em contribuir para a comunidade de código aberto, tornando o nosso trabalho disponível gratuitamente para outros usarem, estudarem e desenvolverem.
+
+O código neste repositório está licenciado sob a GNU General Public License v3.0. Consulte o arquivo `LICENSE` na raiz deste repositório para obter o texto completo da licença.
+
+As fotos, modelos 3D, documentos Word, documentos PowerPoint e PDFs neste repositório estão licenciados sob a Licença Creative Commons Atribuição-CompartilhaIgual 4.0 Internacional. Consulte o arquivo `LICENSE-CC-BY-SA` tambem na raiz deste repositório para obter o texto completo da licença.
--- Raspberry/main.py
+++ Raspberry/main.py
@@ -48,20 +48,67 @@ Bits extra:
     35 -> BIT_EXTRA1_IN no Autómato usado para indicar qual o líquido que está a ser enviado para o autómato e quando começar o programa
     37 -> BIT_EXTRA2_IN no Autómato usado para indicar qual o líquido que está a ser enviado para o autómato e quando começar o programa
         
+Pos Rasp:
+    3 -> "impulso" para fazer o eixo mexer
 """
 
 
-def main():
-    print("main")
 
 if __name__ == "__main__":
     try:
-        server_thread = threading.Thread(target=uvicorn.run, args=(app,), kwargs={"host": "0.0.0.0", "port": 5000}, daemon=True)
+        GPIO.setmode(GPIO.BCM)
+        # Enviar percentagens para o Autómato
+        GPIO.setup(8, GPIO.OUT)
+        GPIO.setup(10, GPIO.OUT)
+        GPIO.setup(12, GPIO.OUT)
+        GPIO.setup(16, GPIO.OUT)
+        GPIO.setup(18, GPIO.OUT)
+        GPIO.setup(22, GPIO.OUT)
+        GPIO.setup(24, GPIO.OUT)
 
+        # Receber percentagens do Autómato
+        GPIO.setup(26, GPIO.IN)
+        GPIO.setup(28, GPIO.IN)
+        GPIO.setup(32, GPIO.IN)
+        GPIO.setup(36, GPIO.IN)
+        #GPIO.setup(38, GPIO.IN)
+        GPIO.setup(40, GPIO.IN)
+
+        # Para drivers do motor (LN298)
+        # Motor 1
+        GPIO.setup(5, GPIO.OUT)
+        GPIO.setup(7, GPIO.OUT)
+        GPIO.setup(11, GPIO.OUT)
+        GPIO.setup(13, GPIO.OUT)
+
+        # Motor 2
+        GPIO.setup(15, GPIO.OUT)
+        GPIO.setup(19, GPIO.OUT)
+        GPIO.setup(21, GPIO.OUT)
+        GPIO.setup(23, GPIO.OUT)
+
+        # Motor 3
+        GPIO.setup(27, GPIO.OUT)
+        GPIO.setup(29, GPIO.OUT)
+        GPIO.setup(31, GPIO.OUT)
+        GPIO.setup(33, GPIO.OUT)
+
+        # Bits extra
+        GPIO.setup(35, GPIO.IN)
+        GPIO.setup(37, GPIO.IN)
+
+        # Posição Raspberry PI (para o eixo)
+        GPIO.setup(3, GPIO.OUT)
+
+
+        server_thread = threading.Thread(target=uvicorn.run, args=(app,), kwargs={"host": "0.0.0.0", "port": 5000}, daemon=True)
         server_thread.start()
+
+
         pins.enviar_armazenamento_para_o_automato()
 
-        main()
+        pins.wait_for_start() 
+
     except KeyboardInterrupt:
         print("\nInterrunpido pelo utilizador")
         sys.exit(0)
